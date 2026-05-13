f584c10d655cacf71d082d3723cb00be12ddd288
FanBingQiang <3581505260@qq.com>
2026-05-13 23:16:58 +0800

    获取新闻详情

--- toutiao_backend/crud/news.py
+++ toutiao_backend/crud/news.py
@@ -1,5 +1,5 @@
-from sqlalchemy import select, func
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func, update
+from sqlalchemy.ext.asyncio import AsyncSession, result
 from models.news import NewsCategory, News
 
 
@@ -20,4 +20,22 @@ async def get_news_count(db: AsyncSession,category_id:int):
     result = await db.execute(stmt)
     return result.scalar_one()
 
+async def get_news_detail(db: AsyncSession,news_id:int):
+    stmt = select(News).where(News.id==news_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+async def increase_news_views(db: AsyncSession,news_id:int):
+    update(News).where(News.id == news_id).values(views=News.views+1)
+    result = await db.execute(update)
+    await db.commit()
+
+    #检查是否真的+1了
+    return result.rowcount > 0
+
+async def get_related_news(db: AsyncSession,category_id:int,news_id:int,limit:int=5):
+    stmt = select(News).where(News.category_id==category_id,News.id != news_id).order_by(News.views.desc(),News.publish_time.desc()).limit(limit)
+    result = await db.execute(stmt)
+    return result.scalars().all()
 
--- toutiao_backend/routers/news.py
+++ toutiao_backend/routers/news.py
@@ -1,3 +1,6 @@
+from email.mime import image
+from fastapi import HTTPException
+
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -23,6 +26,22 @@ async def get_news_list(
     total = await news.get_news_count(db,category_id)
     has_more = (offset + len(news_list))<total
     return {"code" : 200, "message" : "sucess","data":{"list":news_list,"total":total,"hasMore":has_more}}
+
+@router.get("/detail")
+async def get_news_detail(news_id: int = Query(...,alias="id"), db: AsyncSession = Depends(get_db)):
+    #NEWS详情 浏览+1 相关新闻
+    news_detail = await news.get_news(db,news_id)
+    if news_detail is None:
+        raise HTTPException(status_code=404, detail="news not found")
+
+    views_res = await news.increase_news_views(db,news_detail.id)
+    if views_res is None:
+        raise HTTPException(status_code=404, detail="news not found")
+
+    related_news = await news.get_related_news(db,news_detail.id,news_detail.category_id)
+
+    return {"code" : 200, "message" : "sucess","data":{"id":news_detail.id,"title":news_detail.title,"content":news_detail.content,"image" : news_detail.image,"author":news_detail.author,"publishTime":news_detail.publishTime,"categoryId":news_detail.category_id,"view":news_detail.views,"relatedNews":related_news}}
+
 
 
 
